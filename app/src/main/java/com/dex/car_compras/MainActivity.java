51fb829e42584df3160d5ce650503eb455642356
Breno Teixeira <inf.teixeira@gmail.com>
2019-04-30 01:33:01 -0300

    Add Sliders, Registre and Login Activity

--- app/src/main/java/com/dex/car_compras/MainActivity.java
+++ app/src/main/java/com/dex/car_compras/MainActivity.java
@@ -1,55 +1,56 @@
 package com.dex.car_compras;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.dex.car_compras.activity.CadastroActivity;
+import com.dex.car_compras.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         setButtonBackVisible(false);
         setButtonNextVisible(false);
 
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo 1")
-                .description("Descrição 1")
-                .image(R.drawable.um)
+        addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.holo_orange_light)
-                .backgroundDark(android.R.color.holo_orange_dark)
+                .fragment(R.layout.intro_1)
                 .build()
         );
 
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo 2")
-                .description("Descrição 2")
-                .image(R.drawable.dois)
-                .background(android.R.color.holo_orange_light)
-                .backgroundDark(android.R.color.holo_orange_dark)
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_light)
+                .fragment(R.layout.intro_2)
                 .build()
         );
 
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo 3")
-                .description("Descrição 3")
-                .image(R.drawable.tres)
-                .background(android.R.color.holo_orange_light)
-                .backgroundDark(android.R.color.holo_orange_dark)
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.intro_3)
                 .build()
         );
 
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo 4")
-                .description("Descrição 4")
-                .image(R.drawable.quatro)
-                .background(android.R.color.holo_orange_light)
-                .backgroundDark(android.R.color.holo_orange_dark)
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .canGoForward(false)
+                .canGoBackward(false)
+                .fragment(R.layout.intro_cadastro)
                 .build()
         );
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
